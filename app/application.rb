7d78fb50df14f05bcdaf005b1408e5a2858dfffd
Justin <justinms@outlook.com>
2020-03-09 13:47:34 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    if Time.now.hour >= 12
+      resp.write "Good Afternoon!\n"
+    else
+      resp.write "Good Morning!\n"
+    end
+    resp.finish
+  end
+end
